e4739e1400b64a5b5c3d19343c3d0f01e358c8b2
Lance Wicks <lw@judocoach.com>
2017-02-27 23:14:04 +0000

    Initial ics file creation
    First pass at generating a full calendar from Judobase.

---
+++ ijf_ical.pl
@@ -0,0 +1,60 @@
+#!/use/bin/env perl
+use strict;
+use warnings;
+
+use Data::ICal;
+use Data::ICal::Entry::Event;
+use Date::ICal;
+use JSON qw( decode_json );  
+use LWP::Simple;             
+
+my @dates;
+my $calendar = Data::ICal->new();
+my $json;
+
+for my $age (qw/SEN JUN CAD/) {
+    $json
+        = get(
+        "http://data.judobase.org/api/get_json?params[action]=competition.get_list&params[year]=2017&params[id_age]=$age"
+        );
+
+    my $decoded_json = decode_json($json);
+    
+    for my $event (@$decoded_json) {
+        $event->{age} = $age || 'SEN';
+        push @dates, $event;
+    }
+}
+
+@dates = sort { $a->{date_from} cmp $b->{date_from} } @dates;
+
+for my $event (@dates) {
+    my $vevent = Data::ICal::Entry::Event->new;
+
+    $event->{date_from} =~ m{(\d+)/(\d+)/(\d+)};
+    $vevent->add_properties(
+        summary     => $event->{name},
+        description => $event->{name} . ' ('
+            . $event->{country_short} . ') '
+            . $event->{rank_name},
+        dtstart => Date::ICal->new(
+            year  => $1,
+            month => $2,
+            day   => $3,
+            hour  => 10
+        )->ical,
+    );
+
+    $event->{date_to} =~ m{(\d+)/(\d+)/(\d+)};
+    $vevent->add_properties(
+        dtend => Date::ICal->new(
+            year  => $1,
+            month => $2,
+            day   => $3,
+            hour  => 20
+        )->ical
+    );
+    $calendar->add_entry($vevent);
+}
+
+print $calendar->as_string;
