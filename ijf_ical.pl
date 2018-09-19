020254f7e92a15c16f25d890a2f7fb07120f8d8d
Lance Wicks <lw@judocoach.com>
2018-09-19 07:55:50 +0100

    Perltidied + some Perlcritic fixes

--- ijf_ical.pl
+++ ijf_ical.pl
@@ -2,19 +2,19 @@
 use strict;
 use warnings;
 
-use Data::Dumper;
-
+use Carp 'croak';
 use Data::ICal;
 use Data::ICal::Entry::Event;
 use Date::ICal;
 use JSON qw( decode_json );
 use LWP::Simple;
 
+our $VERSION = 1.0000;
 my @dates;
 my $calendar = Data::ICal->new();
 my $json;
 
-for my $year (qw/2018 2019/){
+for my $year (qw/2018 2019/) {
     for my $age (qw/SEN JUN CAD/) {
         $json
             = get('http://data.judobase.org/'
@@ -27,7 +27,7 @@ for my $year (qw/2018 2019/){
 
         my $decoded_json = decode_json($json);
 
-        for my $event (@$decoded_json) {
+        for my $event ( @{$decoded_json} ) {
             $event->{age} = $age || 'SEN';
             push @dates, $event;
         }
@@ -40,13 +40,13 @@ for my $event (@dates) {
     my $vevent = Data::ICal::Entry::Event->new;
 
     my $uid = $event->{name};
-    $uid =~ s/ //g;
+    $uid =~ s/ //smg;
 
-    $event->{date_from} =~ m{(\d+)/(\d+)/(\d+)}
-        or die 'Date did not match';
+    $event->{date_from} =~ m{(\d+)/(\d+)/(\d+)}xms
+        or croak 'Date did not match';
 
     $vevent->add_properties(
-        summary     => $event->{age} . ' ' . $event->{name},
+        summary     => "$event->{age} $event->{name}",
         description => $event->{name} . ' ('
             . $event->{country_short} . ') '
             . $event->{rank_name} . ' ['
@@ -68,11 +68,13 @@ for my $event (@dates) {
             month => $2,
             day   => $3,
             hour  => 10
-        )->ical . $uid . time,
+            )->ical
+            . $uid
+            . time,
     );
 
-    $event->{date_to} =~ m{(\d+)/(\d+)/(\d+)}
-        or die 'Date did not match';
+    $event->{date_to} =~ m{(\d+)/(\d+)/(\d+)}xms
+        or croak 'Date did not match';
 
     $vevent->add_properties(
         dtend => Date::ICal->new(
@@ -86,5 +88,5 @@ for my $event (@dates) {
 }
 
 my $calendar_text = $calendar->as_string;
-$calendar_text =~ s/^$//;
-print $calendar_text;
+$calendar_text =~ s/^$//smx;
+my $ok = print $calendar_text;
